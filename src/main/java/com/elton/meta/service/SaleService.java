d6a94e98c02a915e7174efc96e9eb8923bc4bfe1
eltoncox <elton.unix@gmail.com>
2022-09-09 19:54:47 -0300

    Date select

--- src/main/java/com/elton/meta/service/SaleService.java
+++ src/main/java/com/elton/meta/service/SaleService.java
@@ -1,10 +1,13 @@
 package com.elton.meta.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.elton.meta.entities.Sale;
 import com.elton.meta.repository.SaleRepository;
@@ -15,7 +18,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale>findSales() {
-		return repository.findAll();
+	public Page<Sale>findSales(String minDate, String maxDate, Pageable  pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
